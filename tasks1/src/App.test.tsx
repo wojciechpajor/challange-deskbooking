c7c19b064675bfeb8c21fe9378f8c7e51692c12b
pwojtekp3 <pwojtekp3@gmail.com>
2021-08-12 09:01:09 +0200

    dodano debounce

--- tasks1/src/App.test.tsx
+++ tasks1/src/App.test.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-import delay from "./service/delay";
+const delay= require('./service/delay')
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
 test("testing delay function", () => {
 
 });
--- tasks1/src/Search.js
+++ tasks1/src/Search.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, {useCallback} from 'react'
 import {useState} from "react";
 import {searchSpaces} from "./service/search";
 import delay from "./service/delay";
+import debounce from 'lodash'
 
 const Search = () => {
     const [inputText,setInputText] = useState([])
 
+    const changeDebouncer = (text) => {
+        debounce(changeHandler(text),1000);
+        };
 
+    const changeHandler = (e) => {
 
-    const onChange = (e) => {
     searchSpaces(e)
         .then(delay(500))
         .then((data) => {
@@ -16,22 +20,13 @@ const Search = () => {
         setInputText(data.map(data => (data.name)));
     })
         .catch((message) => {
-        window.alert(message)
-            searchSpaces(e)
-                .then((data) => {
-                    console.log(data);
-                    setInputText(data.map(data => (data.name)));
-                })
-                .catch((message) => {
-                    window.alert(message)
-
-                })
-
+        window.alert("something went wrong message")
+            changeHandler(e);
     })
 
     }
     return <div>
-        <input type="text" onChange={e => onChange(e.target.value)}/>
+        <input type="text" onChange={(e) => changeDebouncer(e.target.value)}/>
         {
             inputText.map(name => (
                 <li className="liStyle">{name}</li>
